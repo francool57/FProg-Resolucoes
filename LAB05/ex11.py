a268d47257e33c88be06b0d60d1e757e4afb0280
xxanny <68930876+francool57@users.noreply.github.com>
2025-09-18 15:26:44 +0100

    Update ex11.py

--- LAB05/ex11.py
+++ LAB05/ex11.py
@@ -1,17 +1,12 @@
 def media_digitos(n):
     soma = 0
-
-    a = n          # Definir a como n para o n continuar como numero inicial
-    l = 0          # Contar a lenght do numero 
-    while a > 0:   # A funcao len(n) nao e possivel com int
-        l += 1
-        a //= 10
+    i = 0
 
     while n > 0:
         last_digit = n % 10
 
         soma += last_digit
         n //= 10
+        i += 1
 
-    return soma/l
-
+    return soma/i
