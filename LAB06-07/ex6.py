3a26cf53de7135cb00326a1aa3bc9cabc93bd3c6
xxanny <68930876+francool57@users.noreply.github.com>
2025-09-18 22:10:41 +0100

    Update ex6.py

--- LAB06-07/ex6.py
+++ LAB06-07/ex6.py
@@ -1,15 +1,8 @@
 def num_para_seq_cod(n):
-    pares = [0, 2, 4, 6, 8]
-    impares = [1, 3, 5, 7, 9]
+    cod = {0:2, 2:4, 4:6, 6:8, 8:0, 1:9, 3:1, 5:3, 7:5, 9:7}
     tup = tuple()
-
+    
     while n > 0:
-        ld = n % 10
-        if ld % 2 == 0:
-            a = pares[pares.index(ld)+1] if ld != 8 else 0
-        else:
-            a = impares[impares.index(ld)-1] if ld != 1 else 9
-        tup += (a,)
+        tup = (cod[n % 10],) + tup
         n //= 10
-
-    return tup[::-1]
+    return tup
