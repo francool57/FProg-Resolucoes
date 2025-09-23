657e20410377ceebff30a941ea43f572f015718f
xxanny <68930876+francool57@users.noreply.github.com>
2025-09-23 12:53:53 +0100

    Update ex10.py

--- LAB06-07/ex10.py
+++ LAB06-07/ex10.py
@@ -9,3 +9,18 @@ def codifica(chars):
             left += j
 
     return left+right
+
+def descodifica(cod):
+    left = cod[0:len(cod)//2]
+    right = cod[len(cod)//2:]
+    res = ''
+    
+    if len(left) == len(right):
+        for n,m in zip(left,right):
+            res += n + m
+    else:
+        for n,m in zip(left, right[1:]):
+            res += n + m
+        res += right[0]
+
+    return res
